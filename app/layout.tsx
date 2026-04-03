ba6a639135f6cd3fa17851032f265874cd2ab725
Ron Turetzky <turetzkyron@gmail.com>
2026-04-02 21:20:09 -0400

    Update browser tab title to Decentral Park

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Decentral Park',
+  description: 'Decentral Park is a NYC-based progressive tech collective.',
 }
 
 export default function RootLayout({
